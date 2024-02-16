f697350ee6d5784c67d9466794ca18fa2cf2d3bb
crin <crinuyi@gmail.com>
2024-02-16 22:14:32 +0100

    created Header, Footer, Home, Contact, About and Layout templates

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
