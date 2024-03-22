1d385f24084d2f9d2e5ee5be5ced44f95cae33e7
crin <crinuyi@gmail.com>
2024-03-22 21:47:17 +0100

    edited css style on wishlist page

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -3,6 +3,7 @@ import { RxCross1 } from "react-icons/rx";
 import Meta from "../components/Meta";
 import Breadcrumbs from "../components/Breadcrumbs";
 import hubbaBubbaImage from "../images/products/hubba-bubba-bubble-tap-sour-blue-raspberry.jpg";
+import { Link } from "react-router-dom";
 
 const Wishlist = () => {
   return (
@@ -14,17 +15,20 @@ const Wishlist = () => {
           <div className="row">
             <div className="col-12 d-flex flex-wrap">
               <div className="wishlist-card col-3 d-flex">
-                <div className="wishlist-card-details">
-                  <img
-                    src={hubbaBubbaImage}
-                    alt="product"
-                    className="img-fluid"
-                  />
-                  <h5 className="product-title">
-                    Hubba Bubba Bubble Tape Sour Blue Raspberry
-                  </h5>
-                  <p className="price">$4.50 USD</p>
+                <div className="product-card d-block">
+                <div>
+                <img src={hubbaBubbaImage} alt="product" />
                 </div>
+                  <div className="product-details">
+                  <Link to="/store/product/:id">
+                    <h6 className="product-title">
+                      Hubba Bubba Bubble Tape Sour Blue Raspberry
+                    </h6>
+                  </Link>
+                  <p className="price-text">$4.50 USD</p>
+                </div>
+                  </div>
+                  
                 <div className="remove-from-wishlist mx-1">
                   <RxCross1 />
                 </div>
